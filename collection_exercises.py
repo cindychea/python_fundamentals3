7f460eb858ba69ad82914f475d021fe4f4d499f7
cindychea <cindy_chea@hotmail.com>
2019-05-08 14:38:45 -0400

    completed exercise0

---
+++ collection_exercises.py
@@ -0,0 +1,28 @@
+# Create Lists
+fav_colors = ['red', 'blue', 'purple', 'seafoam']
+cousin_ages = [31, 33, 25, 35, 22]
+flipped_heads = [True, True, False, False, True]
+fav_artists = ['Travis', 'Bazzi', 'Tinashe']
+
+# Create Dictionaries
+words_defs = {
+    'blossom': 'the state of flowering',
+    'flower': 'the blossom of a plant',
+    'stem': 'the stalk that supports a lead, flower, or fruit'
+}
+movie_years = {
+    'Hitch': 2005,
+    'Over the Hedge': 2006,
+    'Kung Fu Panda': 2008
+}
+cities_pop = {
+    'Toronto': 2.93,
+    'Sydney': 4.627,
+    'Milan': 1.352
+}
+family_ages = {
+    'Amy': 22,
+    'Ida': 31,
+    'Filly': 35,
+    'Huoi': 33
+}
